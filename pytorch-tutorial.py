9d8028add84472174fdfebd13e766d8e3e269768
Douglas <douglaspindler@gmail.com>
2019-11-24 19:42:05 -0300

    first steps

---
+++ pytorch-tutorial.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+import torch
+
+# initializes with whatever values are in memory
+x = torch.empty(5, 3)
+print(x)
+
+x = torch.rand(5, 3)
+print(x)
+
+x = torch.zeros(5, 3, dtype=torch.long)
+print(x)
+
+# initialize with specific data
+x = torch.tensor([5.5, 3])
+print(x)
+
+x = x.new_ones(5, 3, dtype=torch.double)
+print(x)
+
+x = torch.randn_like(x, dtype=torch.float)
+print(x)
+
+print(x.size())
+
+y = torch.rand(5, 3)
+print(x + y)
+
+print(torch.add(x, y))
+
+result = torch.empty(5, 3)
+torch.add(x, y, out=result)
+print(result)
+
+y.add_(x)
+print(y)
+
+print(x[:, 1])
+
+x = torch.randn(4, 4)
+y = x.view(16)
+z = x.view(-1, 8)  # the size -1 is inferred from other dimensions
+print(x.size(), y.size(), z.size())
+
+x = torch.randn(1)
+print(x)
+print(x.item())
+
+a = torch.ones(5)
+print(a)
+
+b = a.numpy()
+print(b)
+
+a.add_(1)
+print(a)
+print(b)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")          # a CUDA device object
+    y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
+    x = x.to(device)                       # or just use strings ``.to("cuda")``
+    z = x + y
+    print(z)
+    print(z.to("cpu", torch.double))       # ``.to`` can also change dtype together!
+
+print(torch.cuda.is_available())
